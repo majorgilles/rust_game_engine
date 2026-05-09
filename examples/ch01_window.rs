4312a565fe233f481abb7a61f45e647d6c261e02
Gilles Major <major.gilles@gmail.com>
2026-05-09 20:23:36 +0200

    Implement initial window setup using winit

---
+++ examples/ch01_window.rs
@@ -0,0 +1,50 @@
+use winit::application::ApplicationHandler;
+use winit::dpi::PhysicalSize;
+// a trait we'll implement
+use winit::event::WindowEvent;
+use winit::event_loop::{ActiveEventLoop, EventLoop};
+use winit::window::{Window, WindowId};
+
+const WIDTH: u32 = 1280;
+const HEIGHT: u32 = 720;
+
+#[derive(Default)]
+struct App {
+    window: Option<Window>,
+}
+
+impl ApplicationHandler for App {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        let window_attributes = Window::default_attributes()
+            .with_title("Rust Game Engine - ch01")
+            .with_inner_size(PhysicalSize::new(WIDTH, HEIGHT));
+
+        self.window = Some(
+            event_loop
+                .create_window(window_attributes)
+                .expect("Failed to create window"),
+        )
+    }
+
+    fn window_event(
+        &mut self,
+        event_loop: &ActiveEventLoop,
+        _window_id: WindowId,
+        event: WindowEvent,
+    ) {
+        match event {
+            WindowEvent::CloseRequested => {
+                event_loop.exit();
+            }
+            _ => {}
+        }
+    }
+}
+
+fn main() {
+    env_logger::init(); // TODO what does this do
+    let event_loop = EventLoop::new().expect("Failed to create event loop");
+    let mut app = App::default();
+
+    event_loop.run_app(&mut app).expect("Event loop crashed.")
+}
