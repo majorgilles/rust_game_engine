5c283d8f682dae5242c4b8f62638e4964d7ed705
Gilles Major <major.gilles@gmail.com>
2026-05-12 00:54:15 +0200

    Refactor vertex data structure to use texture coordinates instead of colors

--- src/mesh.rs
+++ src/mesh.rs
@@ -6,43 +6,39 @@
 
 pub const VERTICES: &[Vertex] = &[
     Vertex {
+        // bottom-left
         position: [-0.5, -0.5, 0.0],
-        color: [1.0, 0.0, 0.0],
+        texture_coordinates: [0.0, 1.0],
     },
     Vertex {
+        // bottom-right
         position: [0.5, -0.5, 0.0],
-        color: [0.0, 1.0, 0.0],
+        texture_coordinates: [1.0, 1.0],
     },
     Vertex {
+        // top-right
         position: [0.5, 0.5, 0.0],
-        color: [0.0, 0.0, 1.0],
+        texture_coordinates: [1.0, 0.0],
     },
     Vertex {
+        // top-left
         position: [-0.5, 0.5, 0.0],
-        color: [1.0, 1.0, 0.0],
-    },
-    Vertex {
-        position: [0.0, 1.0, 0.0],
-        color: [1.0, 1.0, 1.0],
+        texture_coordinates: [0.0, 0.0],
     },
 ];
 
-pub const INDICES: &[u16] = &[
-    0, 1, 2, // square: lower-right triangle
-    0, 2, 3, // square: upper-left triangle
-    2, 4, 3, // roof: counter-clockwise, so it is not culled
-];
+pub const INDICES: &[u16] = &[0, 1, 2, 0, 2, 3];
 
 #[repr(C)]
 #[derive(Copy, Clone, bytemuck::Pod, bytemuck::Zeroable)]
 pub struct Vertex {
     pub position: [f32; 3],
-    pub color: [f32; 3],
+    pub texture_coordinates: [f32; 2], // UV / tex_coords, address we use to read a texel
 }
 
 impl Vertex {
     const ATTRIBS: [wgpu::VertexAttribute; 2] =
-        wgpu::vertex_attr_array![0 => Float32x3, 1 => Float32x3];
+        wgpu::vertex_attr_array![0 => Float32x3, 1 => Float32x2];
 
     pub fn desc() -> wgpu::VertexBufferLayout<'static> {
         wgpu::VertexBufferLayout {
